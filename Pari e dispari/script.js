181991a5e7e6641e1f5ce35f3f21e97014f06c1c
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-02 17:26:44 +0200

    Pari e Dispari: inserito passaggi logici

--- Pari e dispari/script.js
+++ Pari e dispari/script.js
@@ -5,3 +5,38 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 _____________________________________________________________________________________ */
+
+//PASSAGGI LOGICI:
+
+
+//INPUT
+// Chiedi all’utente se sceglie pari o dispari
+
+//Chiedi all’utente di inserire un numero da 1 a 5
+
+
+
+
+//ELABORAZIONE
+//1. generare numero per il computer
+    //usare funzione che genera numero casuale tra 1 e 5
+
+    // salvare il risultato in una variabile
+
+
+//2. somma dei numeri (Somma il numero dell’utente e quello del computer)
+
+
+//3. funzione per dire se è pari o dispari
+    //crea una funzione che prende un numero e restituisce "pari" se % 2 === 0, altrimenti "dispari"
+
+    //applica la funzione alla somma
+
+//4. stabilire chi vince
+    //se il risultato è quello che ha scelto l'utente, allora vince lui
+
+    //altrimenti, vince il computer
+
+
+
+//OUTPUT: stampare num utente e num computer, somma e chi ha vinto
