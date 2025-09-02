310e07273ae2de8bae3a2c9157a37f389bc8a763
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-02 15:47:58 +0200

    INPUT: chiedere all'utente di inserire una parola

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -6,7 +6,7 @@ _______________________________________________________________________*/
 //PASSAGGI LOGICI:
 
 //INPUT: chiedere all'utente di inserire una parola
-
+const word = (prompt("Inserisci una parola"));
 
 //ELABORAZIONE
 //Creare una funzione per capire se la parola inserita è palindroma
