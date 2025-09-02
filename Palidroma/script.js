cd17ff01d938fda80c895701a612277ebaf483a6
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-02 16:36:15 +0200

    aggiunta la parte di ELABORAZIONE con la funzione per invertire la parola e la parte di OUTPUT con il controllo se è palindroma o no

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -10,7 +10,31 @@ const word = (prompt("Inserisci una parola"));
 
 //ELABORAZIONE
 //Creare una funzione per capire se la parola inserita è palindroma
+function reverseWord(word) {
+    //variabile di stringa revertata
+    let revertedWord = "";
+
+    //ciclo la stringa
+    for (let i = word.length - 1; i >=0; i--) {
+       
+        //estraggo la lettere alla posizione i
+        let letteraIesima = word [i];
+
+        revertedWord += letteraIesima;
+
+    }
+    return revertedWord;
+}
+
+//invoco la funzione
+const reversed = reverseWord(word);
 
 
 //OUTPUT: La parola X è palindroma” oppure “La parola X non è palindroma”
+if (word.toLowerCase() === reversed.toLowerCase()) {
+    console.log(("La parola inserita è palindroma"));
 
+    } else {
+        console.log(("La parola inserita NON è palindroma"));
+        
+    }
