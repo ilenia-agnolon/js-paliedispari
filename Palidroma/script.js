eae8ac3e8c0d5fbd33d96b0e745e460de776fcc3
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-02 15:45:28 +0200

    inserito passaggi logici

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -2,3 +2,15 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 _______________________________________________________________________*/
+
+//PASSAGGI LOGICI:
+
+//INPUT: chiedere all'utente di inserire una parola
+
+
+//ELABORAZIONE
+//Creare una funzione per capire se la parola inserita è palindroma
+
+
+//OUTPUT: La parola X è palindroma” oppure “La parola X non è palindroma”
+
